cafa65e30faed6ca5e75eb1e031f194836ff2604
Lucas Lin <chenghsin2007@gmail.com>
2025-06-12 14:29:05 +0800

    further simplified code for ease of learning

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,33 +2,16 @@ import React from "react";
 
 function Home() {
     return (
-        <main className="landing-page">
-            <section className="intro">
-                <h1>Hi! I'm Lucas</h1>
-                <p>Developer</p>
-            </section>
+        <main>
+            <h1>Hi! I'm Lucas</h1>
+            <p>Developer</p>
 
-            <section className="menu">
-                <div className="menu-grid">
-                    <div className="menu-item">
-                        {/* Replace with icon or image */}
-                        <div className="icon-slot" />
-                        <span>About</span>
-                    </div>
-                    <div className="menu-item">
-                        <div className="icon-slot" />
-                        <span>Projects</span>
-                    </div>
-                    <div className="menu-item">
-                        <div className="icon-slot" />
-                        <span>Links</span>
-                    </div>
-                    <div className="menu-item">
-                        <div className="icon-slot" />
-                        <span>Progress</span>
-                    </div>
-                </div>
-            </section>
+            <div className="menu">
+                <div className="item">About</div>
+                <div className="item">Projects</div>
+                <div className="item">Links</div>
+                <div className="item">Progress</div>
+            </div>
         </main>
     );
 }
