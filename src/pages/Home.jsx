837e16561d02677fadd7aaaf415ee5d56edfa0f1
Lucas Lin <chenghsin2007@gmail.com>
2025-06-12 17:09:20 +0800

    added a pop-out for my about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
+
 function Home() {
+    const [showAbout, setShowAbout] = useState(false);
+
     return (
         <main>
-            <h1 className="'text-red-500">Hi! I'm Lucas</h1>
+            <h1>Hi! I'm Lucas</h1>
             <p>Developer</p>
 
             <div className="menu">
-                <div className="item">About</div>
+                <div className="item" onClick={() => setShowAbout(true)}>About</div>
                 <div className="item">Projects</div>
                 <div className="item">Links</div>
                 <div className="item">Progress</div>
             </div>
+
+            {/* Pop up section for About */}
+            {showAbout && (
+                <div className="window">
+                    <div className="window-header">
+                        <span>About Me</span>
+                        <button className="close-btn" onClick={() => setShowAbout(false)}>X</button>
+                    </div>
+                    <div className="window-body">
+                        <div className="about-header">
+                            <img
+                                src="https://via.placeholder.com/80"
+                                alt="Profile"
+                                className="profile-pic"
+                            />
+                            <div className="about-text">
+                                <h2>Fracas</h2>
+                                <p>A passionate developer exploring creative web experiences.</p>
+                            </div>
+                        </div>
+
+                        <div className="about-divider"></div>
+                        <p>Hi! I am Lucas, forever a learner and gamer whilst occasionally being a developer.</p>
+                    </div>
+                </div>
+            )}
+
         </main>
     );
 }
